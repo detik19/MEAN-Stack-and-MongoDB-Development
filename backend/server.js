e1e609e3b71039b6a9f29ffef7b48448f214e644
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2016-11-06 16:13:50 +0700

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo =require('mongodb').MongoClient;
-var database;
+//var mongo =require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+//var database;
+var Message = mongoose.model('Message',{
+	msg:String
+});
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin","*");
@@ -14,16 +18,22 @@ app.use(function(req, res, next) {
 });
 app.post('/api/message', function(req, res) {
 	console.log(req.body);
-	database.collection('messages').insertOne(req.body);
-
+	var message = new Message(req.body);
+	message.save();
 	res.status(200);
 });
 
-mongo.connect("mongodb://localhost:27017/test", function(err, db) {
+function GetMessages() {
+	Message.find({}).exec(function(err, result) {
+		console.log(result);
+	})
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err, db) {
 	if(!err){
 		console.log("we are connected to mongo");
 		//db.collection('messages').insertOne({'msg':'test'});
-		database=db;
+		//database=db;
 	}
 });
 
