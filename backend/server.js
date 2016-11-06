7eb636c1f4fe39d9276a614995fb23bf0778bf3e
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2016-11-06 10:36:34 +0700

    add backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+/**
+ * http://usejsdoc.org/
+ */
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.post('/api/message', function(req, res) {
+	console.log(req.body);
+	res.status(200);
+});
+
+var server = app.listen(8080, function() {
+	console.log('listening on port ',server.address().port);
+});
