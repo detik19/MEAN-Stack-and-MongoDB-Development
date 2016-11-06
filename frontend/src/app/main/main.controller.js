dc3807bdbb905f2323e0d72df5819bbb08d550e3
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2016-11-06 12:07:06 +0700

    send a post from angular to endpoin

--- frontend/src/app/main/main.controller.js
+++ frontend/src/app/main/main.controller.js
@@ -1,11 +1,13 @@
 export class MainController {
-  constructor () {
+  constructor ($http) {
     'ngInject';
+    this.$http = $http;
 
     
   }
   postMessage(){
 	  console.log("post");
+	  this.$http.post('http://localhost:8080/api/message',{msg:this.message});
   }
 
  
