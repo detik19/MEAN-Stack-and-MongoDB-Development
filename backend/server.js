a144540a6da2602a0e737de44b2459e678364e88
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2016-11-06 15:28:44 +0700

    save to mongo from the end point

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var mongo =require('mongodb').MongoClient;
+var database;
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin","*");
@@ -13,7 +14,17 @@ app.use(function(req, res, next) {
 });
 app.post('/api/message', function(req, res) {
 	console.log(req.body);
+	database.collection('messages').insertOne(req.body);
+
 	res.status(200);
+});
+
+mongo.connect("mongodb://localhost:27017/test", function(err, db) {
+	if(!err){
+		console.log("we are connected to mongo");
+		//db.collection('messages').insertOne({'msg':'test'});
+		database=db;
+	}
 });
 
 var server = app.listen(8080, function() {
